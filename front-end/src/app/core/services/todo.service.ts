28a94d79b0a7de2b63e051b020d13404f846a84c
Zhao Hsin Rui (Ray) <bill56523@gmail.com>
2025-03-23 15:23:27 +0800

    feature: Add environment settings to avoid private info.

--- front-end/src/app/core/services/todo.service.ts
+++ front-end/src/app/core/services/todo.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/todo.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  private apiUrl = 'http://localhost:8080/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
